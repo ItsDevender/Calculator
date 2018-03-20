f337d277cd610cf0242b760964f922345f742b10
Singh <017171@NTTDATA.COM>
2018-03-20 13:08:26 +0530

    corrected the whitespaces checkstyle warnings

--- src/main/java/com/javamvn/examples/Calculator.java
+++ src/main/java/com/javamvn/examples/Calculator.java
@@ -1,14 +1,14 @@
 package com.javamvn.examples;
 
 public class Calculator {
-    public int add(int a,int b){
-        return a+b;
+    public int add(int a,int b) {
+        return a + b;
     }
-    public int subtract(int a,int b){
-        return a-b;
+    public int subtract(int a,int b) {
+        return a - b;
     }
 
-    public int multiplication(int a,int b){
-        return a*b;
+    public int multiplication(int a,int b) {
+        return a * b;
     }
 }
